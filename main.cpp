4a837e13953ed0213d09d16f9b873d2e99e63874
DAUNOIS Hugo <hdaunois@glokenspiel.pedago.ipb.fr>
2025-01-21 14:24:04 +0100

    Ajouter un Timer

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 
 #include "mbed.h"
 
+// Blinking rate in milliseconds
+#define BLINKING_RATE     500ms
+using namespace std::chrono;
+
 // Initialise the digital pin LED1 as an output
 #ifdef LED1
     DigitalOut led(LED1);
@@ -14,21 +18,46 @@
 
 InterruptIn sw(BUTTON1);
 
-// Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
 
-void flip()
+Timer t;
+long long unsigned int count_time;
+volatile char flag = 0;
+
+void start()
 {
-    led = !led;
+    led = 1;
+    t.start();
+}
+
+void stop()
+{
+    led = 0;
+    t.stop();
+    count_time = duration_cast<milliseconds>(t.elapsed_time()).count();
+    t.reset();
+    flag = 1;
 }
 
 int main()
 {
-    sw.rise(&flip);
+    sw.rise(&start);
+    sw.fall(&stop);
 
     printf("begin\n");
 
     while (true) {
+        if (flag == 1)
+        {
+            printf("temps appuie:%llu ms\n", count_time);   
+            flag = 0;
+        }
+                     
         ThisThread::sleep_for(BLINKING_RATE);
     }
 }
+
+/* output:
+begin
+temps appuie:145 ms
+
+*/
